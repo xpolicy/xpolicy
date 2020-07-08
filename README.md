0af89026651a825794cd6934f586005c2c987edc
Aiyan Jiang <mr.aiyanj@gmail.com>
2020-07-08 13:41:47 -0400

    Rename

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# ABAC
+# XPolicy
 
-[![CI](https://github.com/aiyan/abac/workflows/ci/badge.svg)](https://github.com/aiyan/abac/actions?query=workflow%3A%22Node.js+CI%22)
-[![Coverage Status](https://coveralls.io/repos/github/aiyan/abac/badge.svg?branch=master&t=P6KFeX)](https://coveralls.io/github/aiyan/abac?branch=master)
+[![CI](https://github.com/aiyan/xpolicy/workflows/ci/badge.svg)](https://github.com/aiyan/xpolicy/actions?query=workflow%3Aci)
+[![Coverage Status](https://coveralls.io/repos/github/aiyan/xpolicy/badge.svg?branch=master&t=P6KFeX)](https://coveralls.io/github/aiyan/xpolicy?branch=master)
+
+```shell script
+npm install xpolicy
+yarn add xpolicy
+```
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const abac = require('../src');
-const { Eq, Any, StartsWith } = abac.rules;
+const xp = require('../src');
+const { Eq, Any, StartsWith } = xp.rules;
 
-describe('abac', () => {
+describe('xpolicy', () => {
   it('correctly enforces a policy', () => {
-    const policy = new abac.Policy({
+    const policy = new xp.Policy({
       id: 1,
       actions: [Any()],
       subjects: [
@@ -18,13 +18,13 @@ describe('abac', () => {
       context: {
         ip: StartsWith('12'),
       },
-      effect: abac.Allow,
+      effect: xp.Allow,
     });
 
-    const enforcer = new abac.Enforcer();
+    const enforcer = new xp.Enforcer();
     enforcer.addPolicy(policy);
 
-    const operation = new abac.Operation({
+    const operation = new xp.Operation({
       action: 'modify',
       subject: {
         role: 'admin',
