a59de7a941890d8f4990cf9e5369a7f3ec6acdae
Aiyan Jiang <mr.aiyanj@gmail.com>
2020-07-29 10:11:20 -0400

    Fix bug with In rule

--- src/builtin/relations.js
+++ src/builtin/relations.js
@@ -4,115 +4,139 @@ const Rule = require('../rule');
 const Enforcer = require('../enforcer');
 const types = require('../type');
 
-module.exports = {
-  Eq: wanted => {
-    types.checkAndWarn.Primitive(wanted);
-    return new Rule({
-      name: 'equal to',
-      validator: given => wanted === given,
-    });
-  },
-  NotEq: wanted => {
-    types.checkAndWarn.Primitive(wanted);
-    return new Rule({
-      name: 'not equal to',
-      validator: given => wanted !== given,
-    });
-  },
-  Greater: threshold => {
-    types.checkAndErr.Number(threshold);
-    return new Rule({
-      name: 'greater than',
-      validator: given => Number.isInteger(given) && given > threshold,
-    });
-  },
-  Less: threshold => {
-    types.checkAndErr.Number(threshold);
-    return new Rule({
-      name: 'less than',
-      validator: given => Number.isInteger(given) && given < threshold,
-    });
-  },
-  GreaterOrEq: threshold => {
-    types.checkAndErr.Number(threshold);
-    return new Rule({
-      name: 'greater than or equal to',
-      validator: given => Number.isInteger(given) && given >= threshold,
-    });
-  },
-  LessOrEq: threshold => {
-    types.checkAndErr.Number(threshold);
-    return new Rule({
-      name: 'less than or equal to',
-      validator: given => Number.isInteger(given) && given <= threshold,
-    });
-  },
-  In: array => {
-    types.checkAndErr.Array(array);
+function Eq(wanted) {
+  types.checkAndWarn.Primitive(wanted);
+  return new Rule({
+    name: 'equal to',
+    validator: given => wanted === given,
+  });
+}
 
-    const validator = test => {
-      for (const rule of array) {
-        if (rule instanceof Rule) {
-          const valid = rule.validate(test);
-          if (valid) return true;
-        } else if (rule.constructor === Object) {
-          const valid = Enforcer.recursivelyValidate(rule, test);
-          if (valid) return true;
-        } else {
-          if (test === rule) return true;
-        }
+function NotEq(wanted) {
+  types.checkAndWarn.Primitive(wanted);
+  return new Rule({
+    name: 'not equal to',
+    validator: given => wanted !== given,
+  });
+}
+
+function Greater(threshold) {
+  types.checkAndErr.Number(threshold);
+  return new Rule({
+    name: 'greater than',
+    validator: given => Number.isInteger(given) && given > threshold,
+  });
+}
+
+function Less(threshold) {
+  types.checkAndErr.Number(threshold);
+  return new Rule({
+    name: 'less than',
+    validator: given => Number.isInteger(given) && given < threshold,
+  });
+}
+
+function GreaterOrEq(threshold) {
+  types.checkAndErr.Number(threshold);
+  return new Rule({
+    name: 'greater than or equal to',
+    validator: given => Number.isInteger(given) && given >= threshold,
+  });
+}
+
+function LessOrEq(threshold) {
+  types.checkAndErr.Number(threshold);
+  return new Rule({
+    name: 'less than or equal to',
+    validator: given => Number.isInteger(given) && given <= threshold,
+  });
+}
+
+function In(array) {
+  types.checkAndErr.Array(array);
+
+  const validator = test => {
+    for (const rule of array) {
+      if (rule instanceof Rule) {
+        const valid = rule.validate(test);
+        if (valid) return true;
+      } else if (rule.constructor === Object) {
+        const valid = Enforcer.recursivelyValidate(rule, test);
+        if (valid) return true;
+      } else {
+        if (test === rule) return true;
       }
+    }
 
-      return false;
-    };
+    return false;
+  };
 
-    return new Rule({
-      name: 'in array',
-      validator,
-    });
-  },
-  NotIn: function (array) {
-    types.checkAndErr.Array(array);
+  return new Rule({
+    name: 'in array',
+    validator,
+  });
+}
 
-    const validator = data => !this.In(array).validate(data);
+function NotIn(array) {
+  types.checkAndErr.Array(array);
 
-    return new Rule({
-      name: 'not in array',
-      validator,
-    });
-  },
-  AllIn: function (array) {
-    types.checkAndErr.Array(array);
-    return new Rule({
-      name: 'all in array',
-      validator: testArray => {
-        return (
-          Array.isArray(testArray) &&
-          // Check that every test element satisfies an element in the target array.
-          testArray.every(testEle => this.In(array).validate(testEle))
-        );
-      },
-    });
-  },
-  StartsWith: start => {
-    types.checkAndWarn.Primitive(start);
-    return new Rule({
-      name: 'starts with',
-      validator: test => test.startsWith(start),
-    });
-  },
-  EndsWith: end => {
-    types.checkAndWarn.Primitive(end);
-    return new Rule({
-      name: 'ends with',
-      validator: test => test.endsWith(end),
-    });
-  },
-  Contains: cont => {
-    types.checkAndWarn.Primitive(cont);
-    return new Rule({
-      name: 'contains',
-      validator: test => test.includes(cont),
-    });
-  },
+  const validator = data => !In(array).validate(data);
+
+  return new Rule({
+    name: 'not in array',
+    validator,
+  });
+}
+
+function AllIn(array) {
+  types.checkAndErr.Array(array);
+  return new Rule({
+    name: 'all in array',
+    validator: testArray => {
+      return (
+        Array.isArray(testArray) &&
+        // Check that every test element satisfies an element in the target array.
+        testArray.every(testEle => In(array).validate(testEle))
+      );
+    },
+  });
+}
+
+function StartsWith(start) {
+  types.checkAndWarn.Primitive(start);
+  return new Rule({
+    name: 'starts with',
+    validator: test => test.startsWith(start),
+  });
+}
+
+function EndsWith(end) {
+  types.checkAndWarn.Primitive(end);
+  return new Rule({
+    name: 'ends with',
+    validator: test => test.endsWith(end),
+  });
+}
+
+function Contains(cont) {
+  types.checkAndWarn.Primitive(cont);
+  return new Rule({
+    name: 'contains',
+    validator: test => test.includes(cont),
+  });
+}
+
+module.exports = {
+  Eq,
+  NotEq,
+  Greater,
+  Less,
+  GreaterOrEq,
+  LessOrEq,
+  In,
+  NotIn,
+  AllIn,
+  StartsWith,
+  EndsWith,
+  Contains,
 };
